9ed96800c593b7ec8e3fd97edab43ee5bc3719dc
SarahLima <sarahcamposflima@gmail.com>
2024-11-14 23:38:15 -0400

    jogo erro segmentacao na coord 10,10

--- log.c
+++ log.c
@@ -28,59 +28,59 @@ FILE* open_file_read(){
     return input;
 }
 
-//Update the file log.txt with the history of the board acoording with the plays until the end of the game
-void edit_history(HISTORY info_plays, int nlin, int nCol){
-
-    FILE* input = open_file_edit();
-    
-    fprintf(input, "\n\n\nChosen coordinate: %d,%d\n\n", info_plays.coordX, info_plays.coordY);
-
-    //Identifying the positions of the columns
-    fprintf(input, "    ");
-
-    for(int cont = 0; cont  < nCol; cont++){
-        
-        if(cont < 9){
-            fprintf(input, " %d  ", cont+1);
-        }
-        else{
-            fprintf(input, " %d ", cont+1);
-        }
-    }
-    putc('\n', input);
-
-
-    for(int i = 0; i < nlin; i++){
-
-        //Identifying the positions of the lines
-        if(i < 9){
-            fprintf(input, "%d  ", i+1);
-        }
-        else{
-            fprintf(input, "%d ", i+1);
-        }
-
-
-        for(int j = 0; j < nCol; j++){
-            putc('|', input);
-       
-            fprintf(input, " %c ", info_plays.mat[i][j]);
-        }
-
-        putc('|', input);
-        putc('\n', input);
-    }
-
-    putc('\n', input);
-    putc('\n', input);
-
-    fclose(input);
+void close_file(FILE *file){
+    fclose(file);
 }
+
+// //Update the file log.txt with the history of the board acoording with the plays until the end of the game
+// void edit_history(HISTORY info_plays, int nlin, int nCol){
+
+//     FILE* input = open_file_edit();
+
+//     //Identifying the positions of the columns
+//     fprintf(input, "    ");
+
+//     for(int cont = 0; cont  < nCol; cont++){
+        
+//         if(cont < 9){
+//             fprintf(input, " %d  ", cont+1);
+//         }
+//         else{
+//             fprintf(input, " %d ", cont+1);
+//         }
+//     }
+//     putc('\n', input);
+
+
+//     for(int i = 0; i < nlin; i++){
+
+//         //Identifying the positions of the lines
+//         if(i < 9){
+//             fprintf(input, "%d  ", i+1);
+//         }
+//         else{
+//             fprintf(input, "%d ", i+1);
+//         }
+
+
+//         for(int j = 0; j < nCol; j++){
+//             putc('|', input);
+       
+//             fprintf(input, " %c ", info_plays.mat[i][j]);
+//         }
+
+//         putc('|', input);
+//         putc('\n', input);
+//     }
+//     putc('\n', input);
+
+//     close_file(input);
+// }
 
 //Update the file log.txt with the template when the game it's over
 void editFinal_history(HISTORY info_plays, int nlin, int nCol){
 
-    FILE* input = fopen("log.txt", "a");
+    FILE* input = open_file_edit();
 
     fprintf(input, "*#*# Game over *#*#\n\n");
 
@@ -125,11 +125,8 @@ void editFinal_history(HISTORY info_plays, int nlin, int nCol){
         putc('|', input);
         putc('\n', input);
     }
-    
-    putc('\n', input);
-    putc('\n', input);
 
-    fclose(input);
+    close_file(input);
 }
 
 //Calculates the game start time
@@ -155,13 +152,40 @@ void login_time(){
 
 }
 
+//Prints the date and time of the game start
 void recordTime(int day, int month, int year, int hour, int min, int sec){
 
-    FILE* input = fopen("log.txt", "w");
+    FILE* input = fopen("log.txt", "w"); //We open with the mode 'w' to refresh the log(since append mode accumulates the history)
     
     fprintf(input, "---Game Start : %d/%d/%d \n", day, month, year);
-    fprintf(input, "time-> %.2d:%.2d:%.2d \n\n", hour, min, sec);
+    fprintf(input, "time-> %.2d:%.2d:%.2d \n\n\n", hour, min, sec);
 
-    fclose(input);
+    close_file(input);
+}
 
+//Prints a message to the player according with the chosen coordinates
+void print_message_history(int valid, int lin){
+
+    FILE* input = open_file_edit();
+
+    if(valid == 1){
+        fprintf(input, "- Digite uma coordenada no formato \"x,y\" que esteja entre 1 e %d:\n", lin);
+    }
+    else if(valid == 2){
+        fprintf(input, "- Essa coordenada já foi escolhida..\n- Por favor, digite outra coordenada  no formato \"x,y\" que esteja dentro do intervalo de 1 a %d!!\n", lin);
+    }
+    else if (!valid){
+        fprintf(input, "- Coordenada inválida!\n- Por favor, digite novamente uma coordenada no formato \"x y\" que esteja dentro do intervalo de 1 a %d!!\n", lin);
+    }
+
+    close_file(input);
+}
+
+void print_chosen_coord(HISTORY info_plays){
+
+    FILE * input = open_file_edit();
+
+    fprintf(input, "Chosen coordinate: %d,%d\n\n\n\n", info_plays.coordX, info_plays.coordY);
+
+    close_file(input);
 }
--- log.h
+++ log.h
@@ -9,6 +9,22 @@ typedef struct history{
 
 }HISTORY;
 
+//LOG FUNCTIONS
+
+//Create a log txt file
+void new_file();
+
+//Open the txt file to read and returns the opened file in mode read
+FILE* open_file_read();
+
+//Open the txt file to edit and returns the opened file in mode append
+FILE* open_file_edit();
+
+//Close the file
+void close_file(FILE *file);
+
+
+
 //Update the file log.txt with the history of the board acoording with the plays until the end of the game
 void edit_history(HISTORY info_plays, int nlin, int nCol);
 
@@ -19,17 +35,12 @@ void editFinal_history(HISTORY info_plays, int nlin, int nCol);
 //Calculates the game start time
 void login_time();
 
-
 //Prints the game start time
 void recordTime(int day, int month, int year, int  hour, int  min, int sec);
 
-//Create a log txt file
-void new_file();
+//Prints a message to the player according with the chosen coordinates int the history
+void print_message_history(int valid, int lin);
 
-//Open the txt file to read
-FILE* open_file_read();
-
-//Open the txt file to edit
-FILE* open_file_edit();
-
+//Print the coordinate chosen by the player in the history
+void print_chosen_coord(HISTORY info_plays);
 
